c5dd34b1da4c20a721934be7ac412a5039377962
Raj <rajmaurya3091@gmail.com>
2026-01-03 23:28:49 +0530

    Added font awesome library and slight upgration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import HomePage from './landing_page/home/HomePage';
+import AboutPage from './landing_page/about/AboutPage';
+import PricingPage from './landing_page/pricing/PricingPage';
+import SupportPage from './landing_page/support/SupportPage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <h1>Hello</h1>
+      <HomePage/>
+      <AboutPage/>
+      <PricingPage/>
+      <SupportPage/>
     </div>
 
   </React.StrictMode>
