a4b4308eb31f06ee60e3dd854d594a19e65fea61
Raj <rajmaurya3091@gmail.com>
2026-01-04 22:00:07 +0530

    zerodha project

--- src/landing_page/home/Awards.js
+++ src/landing_page/home/Awards.js
@@ -1,10 +1,40 @@
 import React from 'react';
 function Awards() {
-    return ( 
-        <h1>
-            Awards
-        </h1>
-     );
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-6 p-5">
+                    <img src="images/largestBroker.svg" alt="largest broker" />
+                </div>
+                <div className="col-6 p-5 mt-5">
+                    <h1>Largest stock broker in India</h1>
+                    <p className= "mb-5">  2+ million Kuberum clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
+                        <div className="row">
+                            <div className="col-6">
+                                <ul>
+                                    <li>
+                                        Futures and Options
+                                    </li>
+                                    <li>
+                                        Commoditiy and derivatives
+                                    </li>
+                                    <li>Currency and derivatives</li>
+                                </ul>
+                            </div>
+                            <div className="col-6">
+                                <ul>
+                                    <li>SIPs</li>
+                                    <li>Stocks & Mutual Funds</li>
+                                    <li>IPOs</li>
+                                </ul>
+                            </div>
+                        </div>
+                         <img src="images/pressLogos.png" alt='press log' style={{width:"90%"}}/>
+                    
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Awards;
--- src/landing_page/home/Stats.js
+++ src/landing_page/home/Stats.js
@@ -1,10 +1,33 @@
 import React from 'react';
 function Stats() {
-    return ( 
-        <h1>
-            Stats
-        </h1>
-     );
+    return (
+        <div className="container mb-3">
+            <div className="row">
+                <div className="col-6">
+                    <h1 className="fs-2 mb-5">Trust with confidence</h1>
+                    <h2 className="fs-4">Customer First Always</h2>
+                    <p className="text-muted"> That's why 1.3+ crore customers trust Kuberum with ₹3.5+ lakh crores worth of equity investments. </p>
+
+                    <h2 className="fs-4">No Spam or gimmicks</h2>
+                    <p className="text-muted"> No gimmicks, spam, "gamification", or annoying push notification, High quality apps that you use at your pace, the way you like. </p>
+
+                    <h2 className="fs-4">We make Your money works for you</h2>
+                    <p className="text-muted"> With initiatives like Nudge and kill Switch, we don't just facilitate transactions, but actively helps you to better with your money. </p>
+
+                    <h2 className="fs-4">The Kuberum Universe</h2>
+                    <p className="text-muted"> Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs. </p>
+                </div>
+                <div className="col-6">
+                    <img src='images/ecosystem.png' alt="ecosystem" style={{ width: "90%" }} />
+                    <div className="text-center">
+                        <a href ="">Explore our products <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>&nbsp;&nbsp;&nbsp;
+                        <a href ="">Try Demo <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default Stats;
