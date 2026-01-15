3910079136546d3f1ce9e56a47add4694550a21a
Raj <rajmaurya3091@gmail.com>
2026-01-15 23:47:40 +0530

    DB-connection-&-Schema-design

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
-const express = require("express");
-const app = express();
-const port = 3200;
+require("dotenv").config();
 
-app.listen(port,(req,res) => {
-    console.log("server chalat hai!");
-})
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3002 ;
+const url = process.env.MONGO_URL;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log("App started");
+    mongoose.connect(url);
+    console.log("DB connected");
+});
